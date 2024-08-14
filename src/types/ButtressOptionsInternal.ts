ea4a65830fdabf77651d78f9f497b4969b78b60d
Tom Cahill <me@tomcahill.co.uk>
2024-08-14 14:10:10 +0100

    TWEAK: Licensing update

--- src/types/ButtressOptionsInternal.ts
+++ src/types/ButtressOptionsInternal.ts
@@ -1,20 +1,21 @@
+/**
+ * Buttress API - The federated real-time open data platform
+ * Copyright (C) 2016-2024 Data People Connected LTD.
+ * <https://www.dpc-ltd.com/>
+ *
+ * This file is part of Buttress.
+ * Buttress is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public Licence as published by the Free Software
+ * Foundation, either version 3 of the Licence, or (at your option) any later version.
+ * Buttress is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public Licence for more details.
+ * You should have received a copy of the GNU Affero General Public Licence along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import Schema from "../model/Schema";
 import {URL} from 'url';
-
-// URL {
-//   href: 'https://www.google.com/',
-//   origin: 'https://www.google.com',
-//   protocol: 'https:',
-//   username: '',
-//   password: '',
-//   host: 'www.google.com',
-//   hostname: 'www.google.com',
-//   port: '',
-//   pathname: '/',
-//   search: '',
-//   searchParams: URLSearchParams {},
-//   hash: ''
-// }
 
 export default interface ButtressOptionsInternal {
   buttressUrl?: string;
